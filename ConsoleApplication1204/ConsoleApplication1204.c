c47009d68b0040c893240703f804efad81ea7a02
nutpizza <werevvolf@gmail.com>
2023-12-04 17:38:03 +0900

    File write and read example
    open a file

--- ConsoleApplication1204/ConsoleApplication1204.c
+++ ConsoleApplication1204/ConsoleApplication1204.c
@@ -1,6 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+#define MAX_SIZE 100000
 
 int main()
 {
+#pragma region FileWrite
+
+	/*
+	FILE* fp = fopen("temp.txt", "w");
+
+	fputs("Hello, World\n", fp);
+
+	fclose(fp);
+	
+	*/
+
+#pragma endregion
+
+#pragma region FileRead
+
+	FILE* fp = fopen("temp.txt", "r");
+	char buffer[MAX_SIZE] = {0, };
+
+	fread(buffer, 1, MAX_SIZE, fp);
+	printf("%s", buffer);
+
+	fclose(fp);
+
+#pragma endregion
+
 	return 0;
 }
